b1a178c4b272161895f52d4187720cc1bd55d4dc
Davi Colzer Lira <davicolzer@gmail.com>
2024-10-30 20:59:44 +0000

    Adiciona teste para atualizar uma ordem

--- src/infrastructure/order/repository/sequelize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequelize/order.repository.spec.ts
@@ -85,4 +85,78 @@ describe('Order repository test', () => {
       ],
     });
   });
+
+  it('Should be able to update a order', async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer('123', 'Customer 1');
+    const address = new Address('Street 1', 1, 'Zipcode 1', 'City 1');
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product('123', 'Product 1', 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      '1',
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order('123', '123', [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ['items'],
+    });
+
+    expect(orderModel.total).toBe(20);
+
+    const itemsToUpdate = orderModel.items.map(
+      (item) =>
+        new OrderItem(
+          item.id,
+          item.name,
+          item.price,
+          item.product_id,
+          item.quantity
+        )
+    );
+
+    const product2 = new Product('456', 'Product 2', 20);
+    await productRepository.create(product2);
+
+    const orderItem2 = new OrderItem(
+      '2',
+      product2.name,
+      product2.price,
+      product2.id,
+      4
+    );
+
+    const allorderItens = [...itemsToUpdate, orderItem2];
+
+    const orderToUpdate = new Order(
+      orderModel.id,
+      orderModel.customer_id,
+      allorderItens
+    );
+
+    await orderRepository.update(orderToUpdate);
+
+    const result = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ['items'],
+    });
+
+    expect(result.total).toBe(120);
+  });
+
+  it.todo('Should be able to find a order');
+  it.todo('Should be able to find all orders');
 });
--- src/infrastructure/order/repository/sequelize/order.repository.ts
+++ src/infrastructure/order/repository/sequelize/order.repository.ts
@@ -4,15 +4,38 @@ import { OrderItemModel } from './order-item.model';
 import { OrderModel } from './order.model';
 
 export class OrderRepository implements OrderRepositoryInterface {
-  update(entity: Order): Promise<void> {
-    throw new Error('Method not implemented.');
+  async update(entity: Order): Promise<void> {
+    await OrderModel.update(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        // include: [{ model: OrderItemModel }],
+
+        where: {
+          id: entity.id,
+        },
+      }
+    );
   }
+
   find(id: string): Promise<Order> {
     throw new Error('Method not implemented.');
   }
+
   findAll(): Promise<Order[]> {
     throw new Error('Method not implemented.');
   }
+
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
